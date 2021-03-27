51c0da349cfdd241f549f23693a87d4c40751987
cris151 <65322213+cris151@users.noreply.github.com>
2021-03-26 20:12:49 -0600

    Create Market.xaml.cs

---
+++ Market.xaml.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Base21
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Market : ContentPage
+    {
+        public Market()
+        {
+            InitializeComponent();
+            btnIP12.Clicked += BtnIP12_Clicked;
+            
+        }
+
+        private void BtnIP12_Clicked(object sender, EventArgs e)
+        {
+            ((NavigationPage)this.Parent).PushAsync(new Page1());
+        }
+    }
+}
